2946b6a26b97299fa4bd777f77fa4792fd2fd28e
Sebastiaan van Steenis <mail@superseb.nl>
2023-03-27 16:05:45 +0200

    Add headers and release timeline table

--- scripts/collect-all-release-notes.sh
+++ scripts/collect-all-release-notes.sh
@@ -1,36 +1,79 @@
 #!/usr/bin/env bash
+> release-notes/README.md
 for minor in v1.2 v1.3 v1.4; do
     product=rke
     > release-notes/${product}-${minor}.md
+    rkeversiontmp=$(mktemp)
+    echo "| Version | Date | US date | EU date |" >> $rkeversiontmp
+    echo "| ------- | ---- | ------- | ------- |" >> $rkeversiontmp
     for patch in $(gh release list -R "rancher/${product}" --exclude-drafts --exclude-pre-releases --limit=1000 | awk -F '\t' '{ print $3 }' | grep ^"${minor}"); do
+        publish_date=$(gh release view "${patch}" -R "rancher/${product}" --json publishedAt -q '.publishedAt' | awk -F'T' '{ print $1 }')
+        echo "| ${patch} | $(date +"%b %d %Y" -d "${publish_date}") | $(date +"%D" -d "${publish_date}") | $(date +"%F" -d "${publish_date}") |" >> $rkeversiontmp
         gh release view "${patch}" -R "rancher/${product}" --json body -q '.body' >> release-notes/${product}-${minor}.md
         echo "-----" >> release-notes/${product}-${minor}.md
     done
+    echo -e "\n\n" >> $rkeversiontmp
+    rketmp=$(mktemp)
+    cat $rkeversiontmp release-notes/${product}-${minor}.md > $rketmp && mv $rketmp release-notes/${product}-${minor}.md
+    cat $rkeversiontmp >> release-notes/README.md
+    echo -e "\n" >> release-notes/README.md
 done
 
 for minor in v2.5 v2.6 v2.7; do
     product=rancher
     > release-notes/${product}-${minor}.md
+    rancherversiontmp=$(mktemp)
+    echo "| Version | Date | US date | EU date |" >> $rancherversiontmp
+    echo "| ------- | ---- | ------- | ------- |" >> $rancherversiontmp
     for patch in $(gh release list -R "rancher/${product}" --exclude-drafts --exclude-pre-releases --limit=1000 | awk -F '\t' '{ print $3 }' | grep ^"${minor}"); do
+        publish_date=$(gh release view "${patch}" -R "rancher/${product}" --json publishedAt -q '.publishedAt' | awk -F'T' '{ print $1 }')
+        echo "| ${patch} | $(date +"%b %d %Y" -d "${publish_date}") | $(date +"%D" -d "${publish_date}") | $(date +"%F" -d "${publish_date}") |" >> $rancherversiontmp
         gh release view "${patch}" -R "rancher/${product}" --json body -q '.body' >> release-notes/${product}-${minor}.md
         echo "-----" >> release-notes/${product}-${minor}.md
     done
+    echo -e "\n\n" >> $rancherversiontmp
+    ranchertmp=$(mktemp)
+    cat $rancherversiontmp release-notes/${product}-${minor}.md > $ranchertmp && mv $ranchertmp release-notes/${product}-${minor}.md
+    cat $rancherversiontmp >> release-notes/README.md
+    echo -e "\n" >> release-notes/README.md
 done
 
 for minor in v1.20 v1.21 v1.22 v1.23 v1.24 v1.25 v1.26; do
     product=rke2
     > release-notes/${product}-${minor}.md
+    rke2versiontmp=$(mktemp)
+    echo "| Version | Date | US date | EU date |" >> $rke2versiontmp
+    echo "| ------- | ---- | ------- | ------- |" >> $rke2versiontmp
     for patch in $(gh release list -R "rancher/${product}" --exclude-drafts --exclude-pre-releases --limit=1000 | awk -F '\t' '{ print $3 }' | grep ^"${minor}"); do
+        publish_date=$(gh release view "${patch}" -R "rancher/${product}" --json publishedAt -q '.publishedAt' | awk -F'T' '{ print $1 }')
+        echo "| ${patch} | $(date +"%b %d %Y" -d "${publish_date}") | $(date +"%D" -d "${publish_date}") | $(date +"%F" -d "${publish_date}") |" >> $rke2versiontmp
+        echo "# Release ${patch}" >> release-notes/${product}-${minor}.md
         gh release view "${patch}" -R "rancher/${product}" --json body -q '.body' >> release-notes/${product}-${minor}.md
         echo "-----" >> release-notes/${product}-${minor}.md
     done
+    echo -e "\n\n" >> $rke2versiontmp
+    rke2tmp=$(mktemp)
+    cat $rke2versiontmp release-notes/${product}-${minor}.md > $rke2tmp && mv $rke2tmp release-notes/${product}-${minor}.md
+    cat $rke2versiontmp >> release-notes/README.md
+    echo -e "\n" >> release-notes/README.md
 done
 
 for minor in v1.20 v1.21 v1.22 v1.23 v1.24 v1.25 v1.26; do
     product=k3s
     > release-notes/${product}-${minor}.md
+    k3sversiontmp=$(mktemp)
+    echo "| Version | Date | US date | EU date |" >> $k3sversiontmp
+    echo "| ------- | ---- | ------- | ------- |" >> $k3sversiontmp
     for patch in $(gh release list -R "k3s-io/${product}" --exclude-drafts --exclude-pre-releases --limit=1000 | awk -F '\t' '{ print $3 }' | grep ^"${minor}"); do
+        publish_date=$(gh release view "${patch}" -R "k3s-io/${product}" --json publishedAt -q '.publishedAt' | awk -F'T' '{ print $1 }')
+        echo "| ${patch} | $(date +"%b %d %Y" -d "${publish_date}") | $(date +"%D" -d "${publish_date}") | $(date +"%F" -d "${publish_date}") |" >> $k3sversiontmp
+        echo "# Release ${patch}" >> release-notes/${product}-${minor}.md
         gh release view "${patch}" -R "k3s-io/${product}" --json body -q '.body' >> release-notes/${product}-${minor}.md
         echo "-----" >> release-notes/${product}-${minor}.md
     done
+    echo -e "\n\n" >> $k3sversiontmp
+    k3stmp=$(mktemp)
+    cat $k3sversiontmp release-notes/${product}-${minor}.md > $k3stmp && mv $k3stmp release-notes/${product}-${minor}.md
+    cat $k3sversiontmp >> release-notes/README.md
+    echo -e "\n" >> release-notes/README.md
 done
